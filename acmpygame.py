91b8bde66db6fc84386d95628d76b9f76d9319d6
J05H14 <j05h14@yahoo.com>
2017-09-27 17:07:56 -0700

    second ball

--- acmpygame.py
+++ acmpygame.py
@@ -12,24 +12,34 @@ background_color = (0, 0, 100)
 
 screen = pygame.display.set_mode(size)
 
-square = pygame.image.load("square.png")
+ball = pygame.image.load("square.png")
+ball2 = pygame.image.load("ball.png")
 
-squareRect = square.get_rect()
+ballRect = ball.get_rect()
+ball2Rect = ball2.get_rect()
 
-speed = [50, 100]
+speed = [25, 50]
+speed2 = [30, 10]
 
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    squareRect = squareRect.move(speed)
+    ballRect = ballRect.move(speed)
+    ball2Rect = ball2Rect.move(speed2)
 
-    if squareRect.left < 0 or squareRect.right > width:
+    if ballRect.left < 0 or ballRect.right > width:
         speed[0] = -speed[0]
-    if squareRect.top < 0 or squareRect.bottom > height:
+    if ballRect.top < 0 or ballRect.bottom > height:
         speed[1] = -speed[1]
 
+    if ball2Rect.left < 0 or ball2Rect.right > width:
+        speed2[0] = -speed2[0]
+    if ball2Rect.top < 0 or ball2Rect.bottom > height:
+        speed2[1] = -speed2[1]
+
     screen.fill(background_color)
-    screen.blit(square, squareRect)
+    screen.blit(ball, ballRect)
+    screen.blit(ball2, ball2Rect)
     pygame.display.flip()
